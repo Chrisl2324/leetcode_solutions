1dc9057fa604310731e4594f73543bc3c45e2991
Christopher Lombardi <cjl273@scarletmail.rutgers.edu>
2023-08-10 19:14:12 -0400

    first commit

---
+++ merge_strings_alternately.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        result = ''
+        i = 0
+        max_length = len(max(word1, word2))
+        while i < max_length:
+            if i < len(word1):
+                result += word1[i]
+            if i < len(word2):
+                result += word2[i]
+            i += 1
+            
+        result += word1[i:]
+        result += word2[i:]
+        return result
+        
+        
+if __name__ == '__main__':
+    sol = Solution()
+    result = sol.mergeAlternately('abcd', 'pq')
+    print(result)
+       
